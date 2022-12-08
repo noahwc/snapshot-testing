ca58e8faf0cf7f019da9f902e14f9204dd5c5c28
Noah Clarke <noah.clarke@referralsaasquatch.com>
2022-12-08 14:54:06 -0800

    more puppeteer setup

--- src/Storyshots.test.js
+++ src/Storyshots.test.js
@@ -1,4 +1,6 @@
 import initStoryshots from "@storybook/addon-storyshots";
 import { imageSnapshot } from "@storybook/addon-storyshots-puppeteer";
 
-initStoryshots({ suite: "Image storyshots", test: imageSnapshot() });
+initStoryshots({
+  test: imageSnapshot({ storybookUrl: "http://localhost:6006/" }),
+});
